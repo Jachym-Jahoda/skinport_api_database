061c8d8270ebee25eb737809328fea6c52d122ad
Jachym <jachym.jahoda@icloud.com>
2025-01-19 18:58:18 +0100

    Updated version.

--- database.go
+++ database.go
@@ -6,12 +6,12 @@ import (
 
 type Item struct {
 	gorm.Model
-	Volume90Day  int
-	Volume30Day  int
-	Name         string
+	Volume90Day  int    `gorm:"uniqueIndex:volume_90_day"`
+	Volume30Day  int    `gorm:"uniqueIndex:volume_30_day"`
+	Name         string `gorm:"uniqueIndex:name"`
 	MarketPage   string
-	Average90Day float64
-	Average30Day float64
-	MinPrice     float64
+	Average90Day float64 `gorm:"uniqueIndex:average_90_day"`
+	Average30Day float64 `gorm:"uniqueIndex:average_30_day"`
+	MinPrice     float64 `gorm:"uniqueIndex:min_price"`
 	Note         string
 }
